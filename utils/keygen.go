b84a443b864b8e2e59bc6bf58d4583bb4afeeb3e
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-09-11 22:07:07 -0300

    Refactor RSA keygen

--- utils/keygen.go
+++ utils/keygen.go
@@ -46,17 +46,18 @@ func KeygenHSM(moduleLocation string, pin string, keyLabel string) {
 	defer p.Logout(session)
 
 	// Generate a key pair (example for RSA)
-	mechanism := []*pkcs11.Mechanism{pkcs11.NewMechanism(keygenMechanism, nil)}
-	_, _, err = p.GenerateKeyPair(session, mechanism, []*pkcs11.Attribute{
+	publicKeyTemplate := []*pkcs11.Attribute{
 		pkcs11.NewAttribute(pkcs11.CKA_MODULUS_BITS, 4096),
 		pkcs11.NewAttribute(pkcs11.CKA_LABEL, []byte(keyLabel)),
 		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, true),
-		// Add other required attributes
-	}, []*pkcs11.Attribute{
+	}
+	privateKeyTemplate := []*pkcs11.Attribute{
 		pkcs11.NewAttribute(pkcs11.CKA_LABEL, []byte(keyLabel)),
 		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, true),
-		// Add required attributes for private key
-	})
+		pkcs11.NewAttribute(pkcs11.CKA_PRIVATE, true),
+	}
+	mechanism := []*pkcs11.Mechanism{pkcs11.NewMechanism(keygenMechanism, nil)}
+	_, _, err = p.GenerateKeyPair(session, mechanism, publicKeyTemplate, privateKeyTemplate)
 	if err != nil {
 		fmt.Printf("Failed to generate key pair: %s\n", err)
 		return
