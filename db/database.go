499e9861425eb1e3796f9841aa74d1434856aafc
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-10-05 00:46:34 -0300

    Add support for db connections

---
+++ db/database.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Name     string `json:"dbname"`
+}
+
+func ConnectDB() *sql.DB {
+	// Open our dbConfig
+	jsonFile, err := os.Open("db/dbConfig.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	// defer the closing of our dbConfig so that we can parse it later on
+	defer jsonFile.Close()
+
+	var dbConfig Config
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &dbConfig)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Password, dbConfig.Name)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/sirupsen/logrus v1.9.3
 
 require (
 	github.com/akamensky/argparse v1.4.0
+	github.com/lib/pq v1.10.9
 	github.com/miekg/pkcs11 v1.1.1
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
